46598e52ae198472f9d72e903c9371422cc9948a
dfinn82 <root@localhost.localdomain>
2022-02-05 09:17:23 -0500

    removed some files

--- decrypt.py
+++
@@ -1,7 +0,0 @@
-# Put your code here
-cipher = input("Enter the coded text:")
-dist = input("Enter the distance value:")
-
-for x in cipher:
-    print(chr(ord(x) - int(dist)),end="")
-    
--- lightyear.py
+++
@@ -1,5 +0,0 @@
-#show how many meters light travels in a year
-seconds = 365 * 24 * (60 ** 2)
-lps = 3 * (10 ** 8)
-
-print("Light travels %d meters in a year." % (lps * seconds))
